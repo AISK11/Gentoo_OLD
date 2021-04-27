ef740f96a06a183544661cba36e926126c977d99
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-04-27 20:29:38 +0000

    Update brightness_decrease.sh

--- scripts/brightness_decrease.sh
+++ scripts/brightness_decrease.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Author: AISK11
-# Description: This script will lover screen brightness by 10%, but prevent 0% brightness.
+# Description: This script will decrease screen brightness by 10%, but prevent 0% brightness.
 # Created for: ~/.config/i3/config:
 # bindsym XF86MonBrightnessUp exec xbacklight -inc 10
 # bindsym XF86MonBrightnessDown exec ~/.config/i3/scripts/brightness_decrease.sh
