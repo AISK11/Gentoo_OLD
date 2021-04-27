71411bcf7084dbd9eca9f405ef52e0dd17f1b889
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-04-27 20:29:17 +0000

    Update and rename brightness.sh to brightness_decrease.sh

--- scripts/brightness_decrease.sh
+++ scripts/brightness_decrease.sh
@@ -4,7 +4,7 @@
 # Description: This script will lover screen brightness by 10%, but prevent 0% brightness.
 # Created for: ~/.config/i3/config:
 # bindsym XF86MonBrightnessUp exec xbacklight -inc 10
-# bindsym XF86MonBrightnessDown exec ~/.config/i3/scripts/brightness.sh
+# bindsym XF86MonBrightnessDown exec ~/.config/i3/scripts/brightness_decrease.sh
 # Dependencies: xbacklight
 
 if [ "$(xbacklight -get | cut -d'.' -f1)" -le "10" ]
