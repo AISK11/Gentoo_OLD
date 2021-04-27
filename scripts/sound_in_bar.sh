cf403d82d81cfbbe9cce17c3f363568ea354313b
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-04-27 21:13:19 +0000

    Update sound_in_bar.sh

--- scripts/sound_in_bar.sh
+++ scripts/sound_in_bar.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+# Author: AISK11
+# Description: This script will decrease screen brightness by 10%, but prevent 0% brightness.
+# Created for: i3bar
+# Dependencies: amixer
+
 AUDIO_MUTED=$(amixer get Master | grep -e "Left.*\[" | cut -d']' -f2 | rev | cut -d'[' -f1 | rev)
 AUDIO_STATUS=$(amixer get Master | grep -e "Left.*\[" | cut -d']' -f1 | rev | cut -d'[' -f1 | rev)
 
