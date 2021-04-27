88ddad590f9416b1de8099bcd417da5d4aa74e05
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-04-27 21:31:40 +0000

    Update mic_in_bar.sh

--- scripts/mic_in_bar.sh
+++ scripts/mic_in_bar.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+# Author: AISK11
+# Description: This script show, if microphone is in use.
+# Created for: i3bar
+# Dependencies: amixer
+
 MIC_STATUS=$(amixer get Capture | grep -e "Left.*\[" | cut -d']' -f2 | rev | cut -d'[' -f1 | rev)
 
 if [ "${MIC_STATUS}" = "off" ]
