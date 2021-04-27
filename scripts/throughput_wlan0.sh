47ee6d9035ecc253344f4de2e985ac5d8b852b5b
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-04-27 19:12:29 +0000

    Update throughput_wlan0.sh

--- scripts/throughput_wlan0.sh
+++ scripts/throughput_wlan0.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Author: AISK11
-# This script calculates network throughput for interface "wlan0" in b/s every second.
+# Description: This script calculates network throughput for interface "wlan0" in b/s every second.
 # 1 MB/s = 8 Mb/s
 # 1 Mb/s = 1,000 kb/s = 1,000,000 b/s
 
