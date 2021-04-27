c23efe5981b2ec003c6adbcdd82ecc7ded43adf9
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-04-27 20:43:36 +0000

    Add files via upload

---
+++ scripts/sound_in_bar.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+AUDIO_MUTED=$(amixer get Master | grep -e "Left.*\[" | cut -d']' -f2 | rev | cut -d'[' -f1 | rev)
+AUDIO_STATUS=$(amixer get Master | grep -e "Left.*\[" | cut -d']' -f1 | rev | cut -d'[' -f1 | rev)
+
+if [ "${AUDIO_MUTED}" = "off" ]
+then
+    echo ""
+else
+    echo "${AUDIO_STATUS}"    
+fi
