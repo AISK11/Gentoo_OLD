c9c49a7a53716d9fc3ff34c20070cf6cd558e7a9
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-04-29 23:39:12 +0000

    Add files via upload

---
+++ scripts/i3blocks_ram.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Author: AISK11
+# Description: This script shows how much RAM is used (buff/cache) in percentage.
+# Created for: i3blocks
+# Dependencies: sys-process/procps
+
+TOTAL_KB=$(free | grep Mem | tr -s ' ' | cut -d' ' -f2)
+USED_KB=$(free | grep Mem | tr -s ' ' | cut -d' ' -f6)
+
+echo "$((${USED_KB} * 100 / ${TOTAL_KB}))%"
