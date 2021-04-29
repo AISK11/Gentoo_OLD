1fe25771aa7c0acfcf6eafbe4b387671a1347928
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-04-29 23:51:55 +0000

    Update i3blocks_audio.sh

--- scripts/i3blocks_audio.sh
+++ scripts/i3blocks_audio.sh
@@ -3,7 +3,7 @@
 # Author: AISK11
 # Description: This script shows current volume or status of the audio.
 # Created for: i3blocks
-# Dependencies: sys-apps/coreutils
+# Dependencies: sys-apps/coreutils sys-apps/grep sys-apps/util-linux
 
 AUDIO_MUTED=$(amixer get Master | grep -e "Left.*\[" | cut -d']' -f2 | rev | cut -d'[' -f1 | rev)
 AUDIO_STATUS=$(amixer get Master | grep -e "Left.*\[" | cut -d']' -f1 | rev | cut -d'[' -f1 | rev)
