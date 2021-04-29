90baa5852217f51084173c0e359a9ae84311e7eb
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-04-29 21:21:33 +0000

    Add files via upload

---
+++ scripts/i3bar_cpu.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Author: AISK11
+# Description: This script shows CPU utilization in percentage.
+# Created for: i3bar
+# Dependencies: app-admin/sysstat
+
+USAGE=$((10000 - $(mpstat 1 1 | tail -n 1 | rev | cut -d' ' -f1 | rev | tr -d .)))
+
+USAGE1=$((${USAGE} / 100))
+USAGE2=$((${USAGE} % 100))
+
+printf "%3d.%02d%%\n" ${USAGE1} ${USAGE2}
