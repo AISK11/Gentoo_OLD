301b08fc0f73d61c91a228de6d7e46fcf22d1f09
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-04-30 12:20:28 +0000

    Add files via upload

--- scripts/i3blocks_ram.sh
+++ scripts/i3blocks_ram.sh
@@ -5,7 +5,17 @@
 # Created for: i3blocks
 # Dependencies: sys-process/procps sys-apps/grep
 
-TOTAL_KB=$(free | grep Mem | tr -s ' ' | cut -d' ' -f2)
-USED_KB=$(free | grep Mem | tr -s ' ' | cut -d' ' -f6)
+# https://github.com/KittyKatt/screenFetch/issues/386#issuecomment-249312716
+# USED_MEM = MemTotal + Shmem - MemFree - Buffers - Cached - SReclaimable
+# values are in kb from /proc/meminfo
 
-echo "$((${USED_KB} * 100 / ${TOTAL_KB}))%"
+MemTotal=$(cat /proc/meminfo | grep MemTotal | tr -s ' ' | cut -d' ' -f2)
+Shmem=$(cat /proc/meminfo | grep Shmem | tr -s ' ' | cut -d' ' -f2)
+MemFree=$(cat /proc/meminfo | grep MemFree | tr -s ' ' | cut -d' ' -f2)
+Buffers=$(cat /proc/meminfo | grep Buffers | tr -s ' ' | cut -d' ' -f2)
+Cached=$(cat /proc/meminfo | grep "^Cached" | tr -s ' ' | cut -d' ' -f2)
+SReclaimable=$(cat /proc/meminfo | grep SReclaimable | tr -s ' ' | cut -d' ' -f2)
+
+USED_MEM=$((${MemTotal} + ${Shmem} - ${MemFree} - ${Buffers} - ${Cached} - ${SReclaimable}))
+
+echo "$((USED_MEM * 100 / MemTotal))%"
