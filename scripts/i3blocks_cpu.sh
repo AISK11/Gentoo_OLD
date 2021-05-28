5ba5f9ba8ac9bd3509b77b9260f851212c81b7b0
AISK11 <60526069+AISK11@users.noreply.github.com>
2021-05-28 20:15:11 +0200

    Update i3blocks_cpu.sh

--- scripts/i3blocks_cpu.sh
+++ scripts/i3blocks_cpu.sh
@@ -11,3 +11,5 @@ USAGE1=$((${USAGE} / 100))
 USAGE2=$((${USAGE} % 100))
 
 printf "%3d.%02d%%\n" ${USAGE1} ${USAGE2}
+
+# grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
